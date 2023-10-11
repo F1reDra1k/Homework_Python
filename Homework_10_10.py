35c24479dafc0013b5cb4d4b2162e6a91338e3b5
F1reDra1k <dhorosenko@gmail.com>
2023-10-11 16:32:44 +0300

    adding lesson 2

--- Homework_10_10.py
+++ Homework_10_10.py
@@ -1,18 +1,13 @@
-import result
 
 try:
     value_int_1 = int(input("Please type a number: "))
-except ValueError:
-         print("It should be a number")
-except ZeroDivisionError:
-         print("You can't divide to 0")
-value_operator = input("Please choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/' \n 5 '**'\nYour answer: ")
-try:
+    value_operator = input("Please choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/' \n 5 '**'\nYour answer: ")
     value_int_2 = int(input("Please type another number: "))
 except ValueError:
          print("It should be a number")
 except ZeroDivisionError:
          print("You can't divide to 0")
+
 
 if value_operator == "1":
     result = value_int_1 + value_int_2
@@ -21,9 +16,15 @@ elif value_operator == "2":
 elif value_operator == "3":
     result = value_int_1 * value_int_2
 elif value_operator == "4":
-    result = value_int_1 / value_int_2
+    try:
+        result = value_int_1 / value_int_2
+    except ValueError:
+        print("It should be a number")
+    except ZeroDivisionError:
+        print("You can't divide to 0")
+
 elif value_operator == "5":
     result = value_int_1 ** value_int_2
 else:print(result)
 
-print(result)
+ print(result)
