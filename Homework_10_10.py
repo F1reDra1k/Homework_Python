e559d29b6222be4f6316a5592e1cecfc4ff479c9
F1reDra1k <dhorosenko@gmail.com>
2023-10-12 18:53:58 +0300

    adding lesson 2

--- Homework_10_10.py
+++ Homework_10_10.py
@@ -1,23 +1,31 @@
 
-# while True:
+while True:
     try:
         value_int_1 = int(input("Please type a number: "))
     except ValueError:
         print("It should be a number")
-    #     continue
-    # break
+        continue
+    break
 
-value_operator = input("Please choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/' \n 5 '**'\nYour answer: ")
-if value_operator > "5":
-print("wrong meaning")
+while True:
+    try:
+        value_operator = input("Please choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/' \n 5 '**'\nYour answer: ")
+        if value_operator not in ["1", "2", "3", "4", "5"]:
+            raise ValueError
 
-# while True:
+    except ValueError:
+        print("Invalid symbol")
+        continue
+    break
+
+
+while True:
     try:
         value_int_2 = int(input("Please type another number: "))
     except ValueError:
         print("It should be a number")
-    #     continue
-    # break
+        continue
+    break
 
 if value_operator == "1":
     result = value_int_1 + value_int_2
