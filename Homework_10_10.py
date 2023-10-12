5820432be00110573cbf2f396c281155e0d2671d
F1reDra1k <dhorosenko@gmail.com>
2023-10-12 21:53:56 +0300

    adding lesson 2

--- Homework_10_10.py
+++ Homework_10_10.py
@@ -1,7 +1,7 @@
 
 while True:
     try:
-        value_int_1 = float(input("Please type a number: "))
+        value_float_1 = float(input("Please type a number: "))
     except ValueError:
         print("It should be a number")
         continue
@@ -20,25 +20,25 @@ while True:
 
 while True:
     try:
-        value_int_2 = float(input("Please type another number: "))
+        value_float_2 = float(input("Please type another number: "))
     except ValueError:
         print("It should be a number")
         continue
     break
 
 if value_operator == "1":
-    result = value_int_1 + value_int_2
+    result = value_float_1 + value_float_2
 elif value_operator == "2":
-    result = value_int_1 - value_int_2
+    result = value_float_1 - value_float_2
 elif value_operator == "3":
-    result = value_int_1 * value_int_2
+    result = value_float_1 * value_float_2
 elif value_operator == "4":
     try:
-        result = value_int_1 / value_int_2
+        result = value_float_1 / value_float_2
     except ZeroDivisionError:
         result = ("You can't divide to 0")
 elif value_operator == "5":
-    result = value_int_1 ** value_int_2
+    result = value_float_1 ** value_float_2
 
 else:print(result)
 
