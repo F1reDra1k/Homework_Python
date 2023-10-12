480e1a15b383940773b63e5b74952322e852de0f
F1reDra1k <dhorosenko@gmail.com>
2023-10-12 18:08:09 +0300

    adding lesson 2

--- Homework_10_10.py
+++ Homework_10_10.py
@@ -1,13 +1,23 @@
 
-try:
-    value_int_1 = int(input("Please type a number: "))
-    value_operator = input("Please choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/' \n 5 '**'\nYour answer: ")
-    value_int_2 = int(input("Please type another number: "))
-except ValueError:
-         print("It should be a number")
-except ZeroDivisionError:
-         print("You can't divide to 0")
+# while True:
+    try:
+        value_int_1 = int(input("Please type a number: "))
+    except ValueError:
+        print("It should be a number")
+    #     continue
+    # break
 
+value_operator = input("Please choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/' \n 5 '**'\nYour answer: ")
+if value_operator > "5":
+print("wrong meaning")
+
+# while True:
+    try:
+        value_int_2 = int(input("Please type another number: "))
+    except ValueError:
+        print("It should be a number")
+    #     continue
+    # break
 
 if value_operator == "1":
     result = value_int_1 + value_int_2
@@ -18,13 +28,11 @@ elif value_operator == "3":
 elif value_operator == "4":
     try:
         result = value_int_1 / value_int_2
-    except ValueError:
-        print("It should be a number")
     except ZeroDivisionError:
-        print("You can't divide to 0")
-
+        result = ("You can't divide to 0")
 elif value_operator == "5":
     result = value_int_1 ** value_int_2
+
 else:print(result)
 
- print(result)
+print(result)
