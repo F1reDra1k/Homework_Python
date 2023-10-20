3e0307a26772bb8c8a295d8f2bd4a2bab93e2e70
F1reDra1k <dhorosenko@gmail.com>
2023-10-20 19:01:09 +0300

    homework_dop_extra

--- homework_extra.py
+++ homework_extra.py
@@ -3,13 +3,22 @@
 # Завдання потрібно виконати через цикл у циклі (див. приклад нижче) та наведення типів.
 # Генерування через range або інші "фішки" можна в якості альтернативного вирішення, але спершу через цикл у циклі))
 
-my_string_1 = "0123456789"
-my_string_2 = "0123456789"
+# my_string_1 = "0123456789"
+# my_string_2 = "0123456789"
 
 # for symb_int_1 in my_string_1:
 #     for symb_int_2 in my_string_2:
 #         print(symb_int_1 + symb_int_2)
 
+my_string = '0123456789'
+
+for i in range(100):
+    number_str = ''
+    if i > 9:
+        number_str += my_string[i // 10]
+    number_str += my_string[i % 10]
+    number = int(number_str)
+    #print(number)
 
 ###################### Приклад ###############################
 
